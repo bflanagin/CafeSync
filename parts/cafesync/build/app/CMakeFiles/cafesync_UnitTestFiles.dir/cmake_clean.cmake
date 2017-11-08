00b60491c69ae9e941d02537a1efa34b29af064f
Benjamin Flanagin <bflanagin@vagueentertainment.com>
2017-11-07 19:33:33 -0600

    stuff

--- parts/cafesync/build/app/CMakeFiles/cafesync_UnitTestFiles.dir/cmake_clean.cmake
+++
@@ -1,5 +0,0 @@
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/cafesync_UnitTestFiles.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- stage/usr/lib/x86_64-linux-gnu/qt5/qml/Ubuntu/Components/Themes/Ambiance/artwork/header_overflow_dropshadow@27.sci
+++
@@ -1,5 +0,0 @@
-border.left: 27
-border.top: 27
-border.bottom: 27
-border.right: 27
-source: header_overflow_dropshadow@27.png
